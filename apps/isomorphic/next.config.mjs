9999ffb0e79353353dd4d5563144f1807538c004
VU QUOC HUNG <vuquochung220402@gmail.com>
2026-03-21 13:06:05 +0700

    Update next.config.mjs

--- apps/isomorphic/next.config.mjs
+++ apps/isomorphic/next.config.mjs
@@ -2,7 +2,7 @@ import './src/env.mjs';
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone',
   images: {
     remotePatterns: [
       {
